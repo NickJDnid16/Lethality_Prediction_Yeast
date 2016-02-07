2e60d758cbf9229c502a1c984b4bccda6b7f16a4
NickJDnid16 <nid16@aber.ac.uk>
2016-02-07 19:24:22 +0000

    Essential Genes

--- 1_Return_Genes_With_Lethalities.py
+++ 1_Return_Genes_With_Lethalities.py
@@ -67,7 +67,7 @@ outputfile.close()
 
 outputfile = open('./Single_Lethality_Genes.txt', mode='w')
 inputfile = open('./Genes_With_All_Lethality.txt', mode='r')
-
+essOutputfile = open('./Lethal_Yeast.txt', mode='w')
 for line in inputfile:
     v = ",viable" in line
     l = ",inviable" in line
@@ -87,6 +87,7 @@ for line in inputfile:
             bit = bits[0]+",inviable\n"
             print (bit)
             outputfile.write(bit)
+            essOutputfile.write(bits[0] + "\n")
         if ((not l) and (not v)):
             print("Not Viable OR Lethal")
 
